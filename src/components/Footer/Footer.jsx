698fedd77bbd1b50aca46441a0b60b01668aef6f
Michael Provoncha <96994466+Mprovonc@users.noreply.github.com>
2023-03-20 14:26:43 +0000

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://michael-provoncha.mprovonc.repl.co'
+      className='link footer__link'
+    >
+      Created By Michael Provoncha<br/>
+    </a>
+    <p>Made with React</p>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.jsx
@@ -0,0 +1,74 @@
+const header = {
+    homepage: 'https://michael-provoncha.mprovonc.repl.co',
+    title: 'MP.',
+  }
+  
+  const about = {
+    name: 'Michael Provoncha',
+    role: 'Front End Developer',
+    description:
+      'I have experience building responsive web applications. Excited for both professional growth and software development, I am mostly self taught with over 1,500 hours of combined learning of front-end languages and frameworks as well as some back-end development. Ready to apply my passion for coding to a talented engineering team to develop quality solutions.',
+    resume: 'https://docs.google.com/document/d/1KzvqLupkrp-gP5v7HL8V2BZBX5JAanOH5va1TrIk1GU/edit?usp=sharing',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/michael-provoncha-3582b4231/',
+      github: 'https://github.com/Mprovonc',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'GitHub Profile Search Engine',
+      description:
+        'Search engine that allows you to search any active profile that has been created on GitHub. Shows users stats such as amount of repositories and follower count.',
+      stack: ['HTML', 'JavaScript'],
+      sourceCode: 'https://github.com/Mprovonc/GitHubProfileSearchEngine',
+      livePreview: 'https://GitHub-Profile-Search-Engine.mprovonc.repl.co',
+    },
+    {
+      name: 'PySimple GUI Calculator',
+      description:
+        'Calculator with implemented GUI which allows for changeable color presets and random color schemes.',
+      stack: ["Python", "PySimpleGui Library"],
+      sourceCode: 'https://github.com/Mprovonc/PySimpleGuiCalculator',
+      livePreview: 'https://replit.com/@Mprovonc/PySimpleGui-Calculator',
+    },
+    {
+      name: 'Random Password Generator',
+      description:
+        'Password generator which allows you to use letters, numbers, and special characters. Can create as many passwords and have any password length.',
+      stack: ['Python'],
+      sourceCode: 'https://github.com/Mprovonc/RandomPasswordGenerator',
+      livePreview: 'https://replit.com/@Mprovonc/Random-Password-Generator',
+    },
+    {
+      name: 'Registration Form',
+      description:
+        'A sample validation form that includes name, email, and password creation.',
+      stack: ['HTML', 'JavaScript'],
+      sourceCode: 'https://github.com/Mprovonc/RegistrationForm',
+      livePreview: 'https://registration-form.mprovonc.repl.co',
+    },
+  ]
+  
+  const skills = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Python',
+    'React',
+    'Node.js',
+    'Three.js',
+    'Tailwind CSS',
+    'SASS',
+    "Rest Api's",
+    'Bootstrap',
+    'Git',
+    'Supabase',
+    'Sanity',
+  ]
+  
+  const contact = {
+    email: 'michael.provoncha@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
