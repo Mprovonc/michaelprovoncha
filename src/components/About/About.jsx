024294a37dc70fcd5175023bb769c962e40fad9f
Michael Provoncha <96994466+Mprovonc@users.noreply.github.com>
2023-03-20 14:43:52 +0000

    build

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,6 @@
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
-import { TypeAnimation } from 'react-type-animation';
 import './About.css'
 
 const About = () => {
